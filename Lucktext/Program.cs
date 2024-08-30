2fada11c5ee43a41e92d55433a56d1e1a3ed85ac
John Dous <john.dous@elev.ga.ntig.se>
2024-08-30 13:57:40 +0200

    första

---
+++ Lucktext/Program.cs
@@ -0,0 +1,75 @@
+﻿string name; 
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("skriv in ditt namn: ");
+name = Console.ReadLine();
+
+// Console.WriteLine("Hello " + "!");
+Console.WriteLine($"Hello {name}!");
+
+string age;
+
+Console.WriteLine("skriv in hur gammal du är: ");
+age = Console.ReadLine();
+
+string city;
+
+Console.WriteLine("vilken stad du bor i: ");
+city = Console.ReadLine();
+
+Console.WriteLine($"Hej jag heter {name} !");
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
